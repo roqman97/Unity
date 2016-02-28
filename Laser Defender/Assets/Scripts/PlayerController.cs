75f8cd0270fab2707897ace6e133b672d159d65a
Taylor <roqman97@live.com>
2016-02-28 12:13:58 -0800

    Rebuilt Laser Defender
    Old version is in _old

--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public float playerSpeed = 1;
+    public float playerSpeed = 15.0f;
+    public float padding = 1f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(playerSpeed * Time.deltaTime, 0, 0);
-        } else if (Input.GetKey(KeyCode.LeftArrow)){
-            transform.position += new Vector3(-playerSpeed * Time.deltaTime, 0, 0);
+    float xmin;
+    float xmax;
+
+    // Use this for initialization
+    void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+            Debug.Log("moving right");
+            Debug.Log(transform.position);
+        }else if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.position += Vector3.left * playerSpeed * Time.deltaTime;
+            Debug.Log("moving left");
+            Debug.Log(transform.position);
         }
+
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 }
---
+++ Laser Defender_archive/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(playerSpeed * Time.deltaTime, 0, 0);
+        } else if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.position += new Vector3(-playerSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
